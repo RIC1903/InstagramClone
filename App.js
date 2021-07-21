365104b6cf4dacc91825c8a753e2fb7775dc6bce
Sairaj Mishra <rickysairaj@gmail.com>
2021-07-21 19:36:48 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {ScrollView,StyleSheet,Text,View} from 'react-native';
+
+
+export default App = () =>{
+  return(
+    <View style={styles.body}>
+      
+    </View>
+  );
+};
+
+const styles= StyleSheet.create({
+  body:{
+    margin: 0;
+  }
+})
