4d6ce3a2025fda806c55f49ba32497208630e06c
Sairaj Mishra <rickysairaj@gmail.com>
2021-07-29 19:50:29 +0530

    Added users reducer and feed redux/actions

--- redux/actions/index.js
+++ redux/actions/index.js
@@ -1,4 +1,4 @@
-import {USER_STATE_CHANGE,USET_POSTS_STATE_CHANGE, USER_FOLLOWING_STATE_CHANGE} from '../constants';
+import {USER_STATE_CHANGE,USET_POSTS_STATE_CHANGE, USER_FOLLOWING_STATE_CHANGE,USERS_DATA_STATE_CHANGE} from '../constants';
 import firebase from 'firebase';
 
 
@@ -38,6 +38,7 @@ export function fetchUserPosts() {
                 })
                 // console.log(posts)
                 dispatch({ type: 'USER_POSTS_STATE_CHANGE', posts })
+
             })
     })
 }
@@ -56,6 +57,59 @@ export function fetchUserFollowing() {
                 })
                 // console.log(following)
                 dispatch({ type: 'USER_FOLLOWING_STATE_CHANGE', following })
+                for(let i =0;i<following.length; i++){
+                    dispatch(fetchUsersData(following[i]));
+                }
+            })
+    })
+}
+
+export function fetchUsersData(uid){
+    return ((dispatch, getState) => {
+        const found = getState().usersState.users.some(el => el.uid === uid);
+
+        if(!found){
+            firebase.firestore()
+            .collection("users")
+            .doc(uid)
+            .get()
+            .then((snapshot) => {
+                if (snapshot.exists) {
+                    let user = snapshot.data();
+                    user.uid=snapshot.id;
+                    dispatch({ type: 'USERS_DATA_STATE_CHANGE', user})
+                    dispatch(fetchUsersFollowingPosts(user.id))
+                }
+                else {
+                    console.log('does not exist')
+                }
+            })
+        }
+    })
+}
+
+export function fetchUsersFollowingPosts(uid) {
+    return ((dispatch,getState) => {
+        firebase.firestore()
+            .collection("posts")
+            .doc(uid)
+            .collection("userPosts")
+            .orderBy("creation", "asc")
+            .get()
+            .then((snapshot) => {
+                
+                console.log(snapshot)
+                console.log({snapshot,uid});
+
+                const user = getState().usersState.users.find(el => el.uid === uid);
+                let posts = snapshot.docs.map(doc => {
+                    const data = doc.data();
+                    const id = doc.id;
+                    return { id, ...data,user }
+                })
+                console.log(posts)
+                dispatch({ type: 'USERS_POSTS_STATE_CHANGE', posts,uid })
+                console.log(getState())
             })
     })
 }
--- redux/reducers/index.js
+++ redux/reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import {user} from './user.js';
+import {users} from './users.js';
 
 const Reducers = combineReducers({
-    userState: user
+    userState: user,
+    usersState: users
 })
 
 export default Reducers;
